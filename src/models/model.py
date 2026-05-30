2be14d5ab93845e83e9604f78d72c531b43c241d
Stephenoco <stephenoconnor748@gmail.com>
2026-05-30 12:16:33 +0100

    feat: Created training notebook and model architecture - model.py     Simple model architecture that will be import into the training notebook. - 03_training.ipynb     Used to train the PitStrategyNet.     The model is then saved to the model directory. - 02_data_processor.ipynb     Fixed data leakage

---
+++ src/models/model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class PitStrategyNet(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
