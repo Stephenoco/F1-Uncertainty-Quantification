feat: Fixed Data Leakages and improved easy of use for running experiments.
- model.py
    Expanded model architecture

- 03_training.ipynb
    Implemented the use of the config.yaml file for easy changing of hyperparameters.

- 04_evaluation.ipynb
    This notebook is used to evaluate the model that has been trained in the 03_training.ipynb notebook.
    The MC dropout is print the top 20 most uncertain laps the model has seen.

- 02_data_processor.ipynb
    Fixed data leakage
    Next update to this notebook will include weather and telemetry data for hopefully better results.

- config.yaml
    This file is used to make changes to the hyperparameters of the model without having to dig through both the 03_training.ipynb & 04_evaluation.ipynb notebooks.

- 02_model_running.py
    This is used to run both the 03_training.ipynb & 04_evaluation.ipynb after making changes to the config.yaml file.

src/models/model.py
@@ -1,16 +1,19 @@
 import torch.nn as nn
 
 class PitStrategyNet(nn.Module):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim, hidden_dim_1=64, hidden_dim_2=32, dropout_rate=0.3):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(input_dim, 128),
+            nn.Linear(input_dim, hidden_dim_1),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128, 64),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim_1, hidden_dim_1),  # extra layer
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(64, 1)
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim_1, hidden_dim_2),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim_2, 1)
         )
 
     def forward(self, x):